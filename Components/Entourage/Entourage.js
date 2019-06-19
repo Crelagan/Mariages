8763a687f056f712ba559108bbf3bc3c627e7e6b
Yann COLIN <yann.colin@niji.fr>
2019-06-19 14:08:29 +0200

    premier jet ecran temoin

--- Components/Entourage/Entourage.js
+++ Components/Entourage/Entourage.js
@@ -1,7 +1,9 @@
 // Components/Entourage.js
 
 import React from 'react'
-import {StyleSheet, View, Button, Text} from 'react-native'
+import {StyleSheet, View, Button} from 'react-native'
+import { Container, Header, Content, List, ListItem, Left, Body, Right, Thumbnail, Text } from 'native-base';
+import entourage from '../../Helpers/Temoins'
 
 const styles = StyleSheet.create({
     main_container: {
@@ -24,12 +26,29 @@ const styles = StyleSheet.create({
 class Entourage extends React.Component {
     render() {
         const {navigate} = this.props.navigation;
+
+        const uri = "https://facebook.github.io/react-native/docs/assets/favicon.png";
         return (
-            <View style={styles.main_container}>
-                <Text style={styles.welcome}>Entourage</Text>
-                <Text style={styles.instructions}>En construction</Text>
-                <Button title='GO!!!' onPress={() => navigate('Dashboard')}/>
-            </View>
+            <Container>
+                <Content>
+                    <List>
+                        {entourage.map(item =>
+                            <ListItem avatar>
+                                <Left>
+                                    <Thumbnail source={require('../../images/01.jpg')} />
+                                </Left>
+                                <Body>
+                                <Text>{item.nom + " " + item.prenom}</Text>
+                                <Text note>{item.mail}</Text>
+                                </Body>
+                                <Right>
+                                    <Text note>{item.role}</Text>
+                                </Right>
+                            </ListItem>
+                        )}
+                    </List>
+                </Content>
+            </Container>
         )
     }
 }
---
+++ Helpers/Temoins.js
@@ -0,0 +1,29 @@
+// Helpers/filmsData.js
+
+export default data = [
+    {
+        nom: "Patate",
+        prenom: "Frite",
+        mail: "vni@gmail.com",
+        role: "Demoiselle d'honneur",
+    },
+    {
+        nom: "Patate",
+        prenom: "Frite",
+        mail: "vni@gmail.com",
+        role: "Temoin"
+    },
+    {
+        nom: "Patate",
+        prenom: "Frite",
+        mail: "vni@gmail.com",
+        role: "Temoin",
+    },
+    {
+        nom: "Patate",
+        prenom: "Frite",
+        mail: "vni@gmail.com",
+        role: "Demoiselle d'honneur"
+    }
+]
+
